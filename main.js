fcd761c3ec5b74819c9158ecd07e481bf503d3ab
iinex <108189440+iinex@users.noreply.github.com>
2022-07-31 21:28:13 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,17 @@ document.getElementById("click_to_convert").addEventListener('click', function()
  window.SpeechRecognition = window.webkitSpeechRecognition;
  const recognition = new SpeechRecognition();
  recognition.interimResults = true;
+
+ recognition.addEventListener('result', e=>{
+         const transcript = Array.from(e.results)
+             .map(result =>result[0])
+             .map(result => result.transcript)
+             .join('');
+console.log(transcript)
+document.getElementById("convert_text").textContent = transcript;
+     })
+
+    if (speech == true){
+    recognition.start();
+ }  
+})
